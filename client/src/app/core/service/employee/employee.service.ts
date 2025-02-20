6074f794c7270206896125cf5090976667a334b6
hiruni-lakshika <155660954+hiruni-lakshika@users.noreply.github.com>
2025-02-20 21:39:51 +0530

    employeeui new changes

--- client/src/app/core/service/employee/employee.service.ts
+++ client/src/app/core/service/employee/employee.service.ts
@@ -13,6 +13,7 @@ export class EmployeeService {
 
   constructor(private http:HttpClient) { }
   getAll(query:string){
+    console.log(this.http.get<Employee[]>(apiurl+query))
     return this.http.get<Employee[]>(apiurl+query)
   }
 
