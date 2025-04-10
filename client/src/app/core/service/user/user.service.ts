b2e6996e7b6d2ca7366dae3432efda65eb429310
hiruni-lakshika <155660954+hiruni-lakshika@users.noreply.github.com>
2025-04-10 22:55:31 +0530

    User component
    User component html and ts files

--- client/src/app/core/service/user/user.service.ts
+++ client/src/app/core/service/user/user.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../../environment";
 import {HttpClient} from "@angular/common/http";
+import {User} from "../../entity/user";
 
 const apiurl = environment.apiUrl+"/users";
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
   constructor(private http: HttpClient) { }
-  getAll(){
-    return this.http.get<UserType[]>(apiurl)
+  getAll(query:string){
+    console.log(apiurl+query);
+    return this.http.get<User[]>(apiurl+query)
   }
 }
